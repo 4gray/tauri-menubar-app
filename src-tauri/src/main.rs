b68ba6ac3aa888e1f1f6949593e75739dba64914
4gray <serega05@gmail.com>
2023-01-17 21:51:45 +0100

    feat: add system tray menu with click listeners

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,46 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
 fn main() {
+    let system_tray_menu = SystemTrayMenu::new();
     tauri::Builder::default()
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a left click");
+            }
+            SystemTrayEvent::RightClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a right click");
+            }
+            SystemTrayEvent::DoubleClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                println!("system tray received a double click");
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
